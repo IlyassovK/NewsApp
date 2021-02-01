3c9400239c7630bd4d66fe7e6e25325f15884b00
Ilyassov Karim <07karimkarim12@gmail.com>
2021-02-01 15:08:44 +0600

    1

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragments/ArticlesFragment.kt
@@ -0,0 +1,7 @@
+package com.androiddevs.mvvmnewsapp.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.androiddevs.mvvmnewsapp.R
+
+class ArticlesFragment: Fragment(R.layout.fragment_article) {
+}
