c58a5e686a2a6b56afca84f847f2627ede79f8bf
Ilyassov Karim <07karimkarim12@gmail.com>
2021-02-01 22:54:24 +0600

    retrofit implementation

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/utils/Utils.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.utils
+
+class Utils {
+    companion object{
+        const val API_KEY: String = "286928f5677543a9b8912ed7f0189b90"
+        const val BASE_URL: String = "https://newsapi.org"
+    }
+}
