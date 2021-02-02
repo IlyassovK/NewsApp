1750f2373ed552a173df14dcd949fc890fc4568f
Ilyassov Karim <07karimkarim12@gmail.com>
2021-02-02 23:06:12 +0600

    RecyclerViewAdapter

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/models/Article.kt
@@ -0,0 +1,25 @@
+package com.androiddevs.mvvmnewsapp.models
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+//Entity in our DataBase
+@Entity(
+    tableName = "articles"
+)
+
+
+data class Article(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int? = null,
+    val author: String,
+    val content: String,
+    val description: String,
+    val publishedAt: String,
+    // room can work only with primitive classes, thus we have create typeconverter for source
+    val source: Source,
+    val title: String,
+    val url: String,
+    val urlToImage: String
+)
