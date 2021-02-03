08709c43a5575e996a16f6437816106647d5894f
Ilyassov Karim <07karimkarim12@gmail.com>
2021-02-03 22:15:01 +0600

    Getting breaking news response

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsViewModel.kt
@@ -0,0 +1,40 @@
+package com.androiddevs.mvvmnewsapp.ui
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.androiddevs.mvvmnewsapp.models.NewsResponse
+import com.androiddevs.mvvmnewsapp.repository.NewsRepository
+import com.androiddevs.mvvmnewsapp.utils.Resource
+import kotlinx.coroutines.launch
+import retrofit2.Response
+
+// we cannot use constructor parameters by default
+// if we need that we must create ViewModelProvideFactory
+// to define how our own ViewModel should be created
+class NewsViewModel(
+    val newsRepository: NewsRepository
+): ViewModel() {
+
+    val breakingNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    var breakingNewPage = 1
+
+    init {
+        getBreakingNews("us")
+    }
+
+    fun getBreakingNews(countryCode: String) = viewModelScope.launch {
+        breakingNews.postValue(Resource.Loading())
+        val response = newsRepository.getBreakingNews(countryCode, breakingNewPage)
+        breakingNews.postValue(handleBreakingNewsResponse(response))
+    }
+
+    private fun handleBreakingNewsResponse(response: Response<NewsResponse>): Resource<NewsResponse> {
+        if(response.isSuccessful){
+            response.body()?.let {resultResponse ->
+                return Resource.Success(resultResponse)
+            }
+        }
+        return Resource.Error(response.message())
+    }
+}
